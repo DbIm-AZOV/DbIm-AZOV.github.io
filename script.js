ae6f7aafc882515818dd23dccc4a8787a19a3ac0
SaikoDmytro <74107512+SaikoDmytro@users.noreply.github.com>
2021-05-28 17:10:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-let item=""  ;
-firstnum="",
-secondnum="",
-resultnum="";
+let item = "" 
+firstnum = "",
+secondnum = "" ,
+resultnum = ""
+num = "";
+
+
+document.getElementById('percent').addEventListener("click", percent);
+document.getElementById('clean').addEventListener("click", clean);
+document.getElementById('plusmin').addEventListener("click", plusmin);
+document.getElementById('divide').addEventListener("click", () => insert('/') );
+document.getElementById('seven').addEventListener("click", () => insert('7'));
+document.getElementById('eight').addEventListener("click", () => insert('8'));
+document.getElementById('nine').addEventListener("click", () => insert('9'));
+document.getElementById('times').addEventListener("click", () => insert('*'));
+document.getElementById('four').addEventListener("click", () => insert('4'));
+document.getElementById('five').addEventListener("click", () => insert('5'));
+document.getElementById('six').addEventListener("click", () => insert('6'));
+document.getElementById('minus').addEventListener("click", () => insert('-'));
+document.getElementById('one').addEventListener("click", () => insert('1') );
+document.getElementById('two').addEventListener("click", () => insert('2'));
+document.getElementById('three').addEventListener("click", () => insert('3'));
+document.getElementById('plus').addEventListener("click", () => insert('+'));
+document.getElementById('zero').addEventListener("click", () => insert('0'));
+document.getElementById('dot').addEventListener("click", () => insert('.'));
+document.getElementById('result').addEventListener("click", result);
 
 function percent(){
-    item=eval(item)/100;
+    item = eval(item) / 100;
     resultitem();
 }
 
 function resultitem(){
-document.form.textview.value=item;}
+    document.form.textview.value = item;}
 
 function insert(num){
     if (item == 0)  {
@@ -21,23 +43,22 @@ function insert(num){
     item+=num; 
     resultitem()
 }
-function clean(){
-    item="0";
-    resultitem();
-    
+ function clean(){
+    item = "0";
+    resultitem();     
 }
-function plusmin(){
-    
+
+function plusmin(){    
     item.indexOf("-") == 0?
-    item=item.replace('-',''):
-    item="-"+ item;
+    item = item.replace('-',''):
+    item = "-" + item;
     resultitem()
 }
 function result(){
 
     if(item){
-        item= eval(item);
-        resultnum=item;
+        item = eval(item);
+        resultnum = item;
     }
    
     resultitem()
